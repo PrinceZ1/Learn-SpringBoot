e671c7c12a1940267aa6841d59dce7a38453053d
PrinceZ1 <nguyen.tt1703@gmail.com>
2025-02-26 13:35:03 +0700

    update

---
+++ src/main/java/com/princez1/learn_springboot/dto/request/AuthRequest.java
@@ -0,0 +1,14 @@
+package com.princez1.learn_springboot.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AuthRequest {
+    String username;
+    String password;
+}
